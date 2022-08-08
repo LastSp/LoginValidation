89d9e5dbe3207cf88b4b8df9e8ddf126281c7ee5
LastSp <lastspaise@gmail.com>
2022-08-08 12:54:25 +0300

    Initial Commit

---
+++ LoginValidation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginValidation
+//
+//  Created by Андрей Колесников on 08.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
